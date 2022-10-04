86211772218ed260402f5db4a50b1e99a5a6ceb9
Zheka <grushevskiy.yevgeniy@mail.ru>
2022-10-04 23:15:51 +0300

    feat: blocked scroll pages

--- online-zoo/pages/desktop_petstory/js/script.js
+++ online-zoo/pages/desktop_petstory/js/script.js
@@ -3,24 +3,21 @@ const humburger = document.querySelector(".humburger"),
     openBurMenu = document.querySelector(".open-bur-menu"),
     popapFon = document.querySelector(".popap-fon");
 
-humburger.addEventListener("click",()=>{
-    if(humburger.classList.contains("humburger_active")){
-        openBurMenu.classList.remove("open-bur-menu_active");
-        popapFon.classList.remove("popap-fon_active");
-    }else{
+humburger.addEventListener("click", () => {
+    if (!humburger.classList.contains("humburger_active")) {
         openBurMenu.classList.add("open-bur-menu_active");
         popapFon.classList.add("popap-fon_active");
         document.body.style.overflow = "hidden";
+        document.body.style.height = "100vh";
     }
 });
 
-[closeBurger,popapFon].forEach(item=>{
-    item.addEventListener("click",(e)=>{
-        if(e.target.classList.contains('popap-fon') || e.target.classList.contains('close-burger')){
+[closeBurger, popapFon].forEach(item => {
+    item.addEventListener("click", (e) => {
+        if (e.target.classList.contains('popap-fon') || e.target.classList.contains('close-burger')) {
             openBurMenu.classList.remove("open-bur-menu_active");
             popapFon.classList.remove("popap-fon_active");
             document.body.style.overflow = "scroll";
         }
     });
 });
-
