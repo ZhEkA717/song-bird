8d1f9e2e59dc435eb6ab29f21d3bb831bbbc64de
Zheka <grushevskiy.yevgeniy@mail.ru>
2022-10-09 18:57:36 +0300

    feat: improve testimonials and pets carousel

--- online-zoo/pages/desktop_petstory/js/script.js
+++ online-zoo/pages/desktop_petstory/js/script.js
@@ -41,11 +41,11 @@ const inputRange = document.querySelector('input[type="range"]');
 const mediaQuery1 = window.matchMedia('(min-width: 1000px)');
 const mediaQuery2 = window.matchMedia('(min-width: 1200px)');
 
-if (mediaQuery1.matches) {
-    userAndUsernameArray.forEach(item => {
-        const newItem = document.createElement("div");
-        newItem.classList.add("testimonials__item");
-        newItem.innerHTML = `
+// if (mediaQuery1.matches) {
+userAndUsernameArray.forEach(item => {
+    const newItem = document.createElement("div");
+    newItem.classList.add("testimonials__item");
+    newItem.innerHTML = `
             <div class="testimonials__item__user">
                 <img src=${item[0]} alt="user4">
                 <div class="testimonials__item__user__descr">
@@ -64,33 +64,47 @@ if (mediaQuery1.matches) {
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae nam voluptatibus architecto
             </div>
         `;
-        testimonialsItems.append(newItem);
-    });
-}
+    testimonialsItems.append(newItem);
+});
+// }
 
 inputRange.addEventListener("input", () => {
     let scrollingWidth = testimonialsItems.scrollWidth;
-    let lastLeftScroll = scrollingWidth - testimonialsItems.offsetWidth,
-        inputRangeMax = Math.ceil((lastLeftScroll * 100) / scrollingWidth);
-    inputRange.max = inputRangeMax;
-    testimonialsItems.scrollLeft = inputRange.value * lastLeftScroll / inputRangeMax;
+    let lastLeftScroll = scrollingWidth - testimonialsItems.offsetWidth;
+    testimonialsItems.scroll({
+        left: inputRange.value * lastLeftScroll / inputRange.max,
+        behavior: "smooth"
+    })
 });
+
+inputRange.addEventListener("touchstart", () => {
+    testimonialsItems.removeEventListener("scroll", fingerScroll);
+});
+inputRange.addEventListener("mouseover", () => {
+    testimonialsItems.removeEventListener("scroll", fingerScroll);
+});
+
+
+testimonialsItems.addEventListener("touchstart", (EO) => {
+    EO.currentTarget.addEventListener("scroll", fingerScroll);
+})
+testimonialsItems.addEventListener("mouseover", (EO) => {
+    EO.currentTarget.addEventListener("scroll", fingerScroll);
+})
 
 testimonialsItems.addEventListener("scroll", fingerScroll);
 
 function fingerScroll() {
     let scrollingWidth = testimonialsItems.scrollWidth;
-    let raz = testimonialsItems.scrollLeft + testimonialsItems.offsetWidth,
-        lastLeftScroll = scrollingWidth - testimonialsItems.offsetWidth,
-        inputRangeMax = Math.ceil((lastLeftScroll * 100) / scrollingWidth);
-    inputRange.max = inputRangeMax;
-
-    const leftScroll = Math.ceil((testimonialsItems.scrollLeft * 100) / scrollingWidth);
-
-    if (raz == scrollingWidth) {
-        inputRange.value = inputRangeMax;
-    } else {
-        inputRange.value = leftScroll;
+    let raz = testimonialsItems.scrollLeft + testimonialsItems.offsetWidth;
+    let lastLeftScroll = scrollingWidth - testimonialsItems.offsetWidth;
+    const leftScroll = Math.floor((testimonialsItems.scrollLeft * 70) / lastLeftScroll) / 10;
+    if (Number.isInteger(leftScroll)) {
+        if (raz == scrollingWidth) {
+            inputRange.value = inputRange.max;
+        } else {
+            inputRange.value = leftScroll;
+        }
     }
 }
 fingerScroll();
@@ -158,11 +172,10 @@ function handleDesktop(e) {
     }
 }
 handleDesktop(mediaQuery3);
-
 //Slider
 document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("resize", resizeSlider);
-    const mediaQuerySlider = window.matchMedia("(min-width: 640px)")
+    const mediaQuerySlider = window.matchMedia("(min-width: 320px)")
 
     let index = 1,
         widthSlides = 0;
@@ -170,144 +183,183 @@ document.addEventListener("DOMContentLoaded", () => {
         slidesBox = document.querySelector(".slider-inner"),
         slides = document.querySelectorAll(".animals__items"),
         buttons = document.querySelectorAll(".buttons_circle");
-        
+
 
     function resizeSlider() {
         if (mediaQuerySlider.matches) {
             widthSlides = container.offsetWidth;
-            slidesBox.style.transform = "translateX(" + (-index*widthSlides) + "px)";
+            slidesBox.style.transform = "translateX(" + (-index * widthSlides) + "px)";
         }
     }
 
     resizeSlider();
 
-    buttons.forEach(item => {    
-        function slider() {
-            slidesBox.style.transform = "translateX(" + (-index * widthSlides) + "px)";
-            slidesBox.style.transition = "transform 1s ease-in-out";
-        }
-        function debounceSerie(func,interval,immediate) {
-            let timer;
-            return function() {
-                let context=this, args=arguments;
-                let later=function() {
-                    timer=null;
-                    if ( !immediate )
-                        func.apply(context,args);
-                };
-                let callNow=immediate&&!timer;
-                clearTimeout(timer);
-                timer=setTimeout(later,interval);
-                if ( callNow )
-                    func.apply(context,args);
+    function slider() {
+        slidesBox.style.transform = "translateX(" + (-index * widthSlides) + "px)";
+        slidesBox.style.transition = "transform 1s ease-out";
+    }
+    let arr = [0];
+    const obj = {
+        1: ["Giant Pandas", "Native to Southwest China", "banana"],
+        2: ["Eagles", "Native to South America", "meet"],
+        3: ["Gorillas", "Native to Congo", "banana"],
+        4: ["Cheetahs", "Native to Africa", "meet"],
+        5: ["Two-Toed Sloth", "Mesomerica, South America", "banana"],
+        6: ["Penguins", "Native to Antarctica", "meet"],
+        7: ["Polar Bear", "Native to Antarctica", "meet"],
+        8: ["Wolf", "Native to Europe and Asia", "meet"],
+        9: ["Headgehog", "Native to Europe", "cherui"],
+        10: ["Raccoon", "Native to Europe", "cherui"],
+        11: ["Kangaroo", "Native to Australia", "grass"],
+        12: ["Lion", "Native to Africa", "meet"],
+        13: ["Leopard", "Native to Africa", "meet"],
+        14: ["Fox", "Native to Europe and Asia", "meet"],
+        15: ["Brown Bear", "Native to Europe and Asia", "berries"],
+        16: ["Deer", "Native to Europe and Asia", "grass"],
+        17: ["Panther", "Native to Australia", "meet"],
+        18: ["Elephant", "Native to Africa", 'grass'],
+        19: ["Giraffe", "Native to Africa", 'grass'],
+        20: ["Hippo", "Native to Africa", 'grass'],
+        21: ["Ostrich", "Native to Africa", "grass"],
+        22: ["Alligator", "Native to Southeastern U. S.", "meet"],
+        23: ["Goat", "Native to North Africa", "grass"],
+        24: ["Peacock", "Native to India", "cherui"],
+        25: ["Wild boar", "Native to Belarus", "acorn"]
+    }
+    function debounceSerie(func, interval, immediate) {
+        let timer;
+        return function () {
+            let context = this,
+                args = arguments;
+            let later = function () {
+                timer = null;
+                if (!immediate)
+                    func.apply(context, args);
             };
+            let callNow = immediate && !timer;
+            clearTimeout(timer);
+            timer = setTimeout(later, interval);
+            if (callNow)
+                func.apply(context, args);
         };
+    };
+    function generateCard(num) {
+        slides[num].innerHTML = "";
+        for (let i = 1; i <= 6; i++) {
+            slides[num].innerHTML += `
+            <div class="animals__item">
+                <div class="animals__item__img">
+                    <img src="assets/images/slider/image${arr[i]}.png" alt="animal${i}">
+                    <div></div>
+                </div>
+                <div class="animals__item__content">
+                    <div class="animals__item__content__title">
+                        <span>${obj[`${arr[i]}`][0]}</span><br>
+                        ${obj[`${arr[i]}`][1]}
+                    </div>
+                    <div class="animals__item__content__img animals__item__content__img_banana">
+                        <img src="assets/images/slider/${obj[`${arr[i]}`][2]}.png" alt="banana-bamboo">
+                    </div>
+                </div>
+            </div>
+        `;
+        }
+    }
 
-        item.addEventListener("click", debounceSerie(startSlider,1000,true));
+    buttons.forEach(item => {
+        item.addEventListener("click", debounceSerie(startSlider, 1000, true));
 
-        function startSlider(EO){
-            const arr = [0];
-            const obj = {
-                1:["Giant Pandas","Native to Southwest China","banana"],
-                2:["Eagles","Native to South America","meet"],
-                3:["Gorillas","Native to Congo","banana"],
-                4:["Cheetahs","Native to Africa","meet"],
-                5:["Two-Toed Sloth","Mesomerica, South America","banana"],
-                6:["Penguins","Native to Antarctica","meet"],
-                7:["Polar Bear","Native to Antarctica","meet"],
-                8:["Wolf","Native to Europe and Asia","meet"],
-                9:["Headgehog","Native to Europe","cherui"],
-                10:["Raccoon","Native to Europe","cherui"],
-                11:["Kangaroo","Native to Australia","grass"],
-                12:["Lion","Native to Africa","meet"],
-                13:["Leopard","Native to Africa","meet"],
-                14:["Fox","Native to Europe and Asia","meet"],
-                15:["Brown Bear","Native to Europe and Asia","berries"],
-                16:["Deer","Native to Europe and Asia","grass"],
-                17:["Panther","Native to Australia","meet"],
-                18:["Elephant","Native to Africa",'grass'],
-                19:["Giraffe","Native to Africa",'grass'],
-                20:["Hippo","Native to Africa",'grass'],
-                21:["Ostrich","Native to Africa","grass"],
-                22:["Alligator","Native to Southeastern U. S.","meet"],
-                23:["Goat","Native to North Africa","grass"],
-                24:["Peacock","Native to India","cherui"],
-                25:["Wild boar","Native to Belarus","acorn"]
-            }
-            while(arr.length < 26){
+        function startSlider(EO) {
+            arr = [0]
+            while (arr.length < 26) {
                 let r = Math.floor(Math.random() * 25) + 1;
-                if(arr.indexOf(r) === -1){
+                if (arr.indexOf(r) === -1) {
                     arr.push(r);
                 }
             }
-            
-            function generateCard(num){
-                slides[num].innerHTML="";
-                for(let i = 1; i<=6; i++){
-                    slides[num].innerHTML += `
-                    <div class="animals__item">
-                        <div class="animals__item__img">
-                            <img src="assets/images/slider/image${arr[i]}.png" alt="animal${i}">
-                            <div></div>
-                        </div>
-                        <div class="animals__item__content">
-                            <div class="animals__item__content__title">
-                                <span>${obj[`${arr[i]}`][0]}</span><br>
-                                ${obj[`${arr[i]}`][1]}
-                            </div>
-                            <div class="animals__item__content__img animals__item__content__img_banana">
-                                <img src="assets/images/slider/${obj[`${arr[i]}`][2]}.png" alt="banana-bamboo">
-                            </div>
-                        </div>
-                    </div>
-                `;
-                }
-            }
-    
+
             if (EO.target.classList.contains("buttons_circle_left") ||
                 EO.target.parentNode.classList.contains("buttons_circle_left")) {
                 index <= 0 ? false : index--;
-                setTimeout(()=>{generateCard(0)},10);
+                setTimeout(() => {generateCard(0)}, 10);
                 slider();
             } else if (EO.target.classList.contains("buttons_circle_right") ||
                 EO.target.parentNode.classList.contains("buttons_circle_right")) {
                 index >= slides.length - 1 ? false : index++;
-                setTimeout(()=>{generateCard(2)},10);
+                setTimeout(() => {generateCard(2)}, 10);
                 slider();
             }
-            setTimeout(()=>{generateCard(1)},1000); 
+            setTimeout(() => {generateCard(1)}, 1000);
         }
     });
 
+    const mediaQueryTouches = window.matchMedia("(max-width: 635px)")
 
+    if(mediaQueryTouches.matches){
+        const animalsBlock = document.querySelector('.animals');
+        animalsBlock.addEventListener('touchstart', funTouchStart, false);
+        let move = "stop";
+    
+        function funTouchStart(EO) {
+            EO = EO || window.event;
+            EO.preventDefault();
+            var touchInfoStart = EO.targetTouches[0];
+            var touchXs = touchInfoStart.pageX;
+            var touchYs = touchInfoStart.pageY;
+    
+            animalsBlock.addEventListener('touchmove', funTouchMove);
+    
+            function funTouchMove(EO) {
+                EO = EO || window.event;
+                EO.preventDefault();
+                var touchInfoMove = EO.targetTouches[0];
+                var touchX1 = touchInfoMove.pageX;
+                var touchY1 = touchInfoMove.pageY;
+                var touchXm = touchX1;
+                var touchYm = touchY1;
+    
+                var minSwipe = 40;
+    
+                if (touchXs > touchXm && touchXs - touchXm >= minSwipe) {
+                    if (Math.abs(touchXs - touchXm) > Math.abs(touchYs - touchYm)) {
+                        move = 'left';
+                    }
+                } else if (touchXs < touchXm && touchXm - touchXs >= minSwipe) {
+                    if (Math.abs(touchXs - touchXm) > Math.abs(touchYs - touchYm)) {
+                        move = 'right';
+                    }
+                }
+            }
+            setTimeout(() => {
+                animalsBlock.removeEventListener('touchmove', funTouchMove, false);
+            }, 500);
+        }
+        animalsBlock.addEventListener('touchend', funTouchEnd, false);
+    
+        function funTouchEnd(EO) {
+            EO = EO || window.event;
+            if (move == "left") {
+                index >= slides.length - 1 ? false : index++;
+                slider();
+            } else if (move == "right") {
+                index <= 0 ? false : index--;
+                slider();
+            }
+            move = "stop";
+        }
+    }
+  
     slidesBox.addEventListener('transitionend', () => {
-      setTimeout(()=>{
-        if (index == slides.length - 1) {
-            index = 1;
-            slidesBox.style.transition = "none";
-            slidesBox.style.transform = "translateX(" + (-index * widthSlides) + "px)";
-        } else if (index == 0) {
-            index = slides.length - 2;
-            slidesBox.style.transition = "none";
-            slidesBox.style.transform = "translateX(" + (-index * widthSlides) + "px)";
-        }
-      },0);
+        setTimeout(() => {
+            if (index == slides.length - 1) {
+                index = 1;
+                slidesBox.style.transition = "none";
+                slidesBox.style.transform = "translateX(" + (-index * widthSlides) + "px)";
+            } else if (index == 0) {
+                index = slides.length - 2;
+                slidesBox.style.transition = "none";
+                slidesBox.style.transform = "translateX(" + (-index * widthSlides) + "px)";
+            }
+        }, 0);
     });
-
 });
-
-// function removeTransition(){
-//     document.querySelectorAll(".animals__item__img").forEach(item=>{
-//         item.querySelectorAll("img").forEach(item=>{
-//             item.style.transition = "1s";
-//         });
-//         item.querySelectorAll("div").forEach(item=>{
-//             item.style.transition = "1s";
-//         });
-//     });
-//     document.querySelectorAll(".animals__item__content__title").forEach(item=>{
-//         item.style.transition = "1s";
-//     });
-// }
-
-// removeTransition();
